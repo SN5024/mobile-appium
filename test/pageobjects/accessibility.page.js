fa20051f6204ac1af2ba92d626bf9287dbf399d0
SN5024 <srishtin@gmail.com>
2026-01-10 12:30:37 -0600

    Add Accessibility Menu Test

---
+++ test/pageobjects/accessibility.page.js
@@ -0,0 +1,33 @@
+export default class AccessibilityPage {
+
+    // Main menu option
+    get accessibilityMenu() {
+        return $('~Accessibility');
+    }
+
+    // Sub-menu option
+    get nodeQueryingOption() {
+        return $('~Accessibility Node Querying');
+    }
+
+    // list under Accessibility Node Querying
+    get nodeQueryingList() {
+        return $('~Task Take out Trash');
+    }
+
+    async openAccessibility() {
+        await this.accessibilityMenu.waitForDisplayed();
+        await this.accessibilityMenu.click();
+    }
+
+    async openNodeQuerying() {
+        await this.nodeQueryingOption.waitForDisplayed();
+        await this.nodeQueryingOption.click();
+    }
+
+    async verifyTaskTakeOutTrash() {
+        await this.nodeQueryingList.waitForDisplayed();
+       // return await this.nodeQueryingList.getText(); for Get Text from element
+       return await this.nodeQueryingList.getAttribute('content-desc');
+    }
+}
--- test/specs/views.e2e.js
+++ test/specs/views.e2e.js
@@ -1,13 +1,13 @@
 import ViewsPage from '../pageobjects/views.page.js';
 import { expect } from 'chai';
 
-describe('ApiDemos Views Tests', () => {
+describe('ApiDemos Views Menu Tests', () => {
     let viewsPage;
 
     before(async () => {
         viewsPage = new ViewsPage();
     });
-    
+
     it('should open Animation from Views', async () => {
         await viewsPage.openViews();
         await viewsPage.openAnimation();
@@ -15,5 +15,14 @@ describe('ApiDemos Views Tests', () => {
         await browser.pause(500); // tiny wait for screen transition
         const animationList = await $('android.widget.TextView'); // first element in Animation screen
         expect(await animationList.isDisplayed()).to.be.true;
+    });
+    
+    it('should open Controls from Views', async () => {
+        await driver.back();
+        await viewsPage.openControls();
+        // Verify that the Contorl screen opened by checking a known element
+        await browser.pause(500); // tiny wait for screen transition
+        const controlList = await $('android.widget.TextView'); // first element in Animation screen
+        expect(await controlList.isDisplayed()).to.be.true;
     });
 });
